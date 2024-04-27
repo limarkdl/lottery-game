b4f0298a96315946ad53c42a60da41805875ab36
limarkdl <ikostin@uclan.ac.uk>
2024-04-27 08:35:40 +0300

    PostCSS + AutoPrefixer

---
+++ postcss.config.ts
@@ -0,0 +1,10 @@
+import autoprefixer from 'autoprefixer';
+
+// eslint-disable-next-line
+const config: any = {
+    plugins: [
+        autoprefixer,
+    ]
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path';
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +10,11 @@ export default defineConfig({
       "@": resolve(__dirname, "src/"),
     },
   },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({})
+      ],
+    }
+  }
 })
